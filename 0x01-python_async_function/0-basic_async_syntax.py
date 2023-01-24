ac8a22b9c3e6340ff7d53148ebf7751cb1995a69
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-24 21:47:18 +0100

    Basics

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,5 +8,5 @@ async def wait_random(max_delay: int = 10) -> float:
     
     """return random wait"""
     ran_wait = random.uniform(0, max_delay)
-    await asyncio.sleep(run_wait)
+    await asyncio.sleep(ran_wait)
     return ran_wait
