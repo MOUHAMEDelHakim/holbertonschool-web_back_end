a914b970edbaf7d87042115ddc7462d90810721c
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-26 22:12:59 +0100

    str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""" Write a type-annotated function 
+that takes a float n as argument 
+"""
+def to_str(n: float) -> int:
+    """
+    returns the string representation of the float.
+    """
+    return int(n)
