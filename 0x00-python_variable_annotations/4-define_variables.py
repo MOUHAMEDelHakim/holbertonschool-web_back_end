9376f07b7f9f4fb1c99fb00c75d18df3c7cec5b6
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-26 22:26:17 +0100

    Define Variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -4,5 +4,5 @@ Define and annotate the following variables with the specified
 """
 a: int = 1
 pi: float = 3.15
-i_understood_annotations = True
+i_understood_annotations:  bool = True
 school: str = "Holberton"
