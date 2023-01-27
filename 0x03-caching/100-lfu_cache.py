4588d58a9a922f03e9d71ccaea50f7561f2f12bb
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-27 23:58:57 +0100

    LFU

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -47,3 +47,11 @@ class LFUCache(BaseCaching):
             self._remove(key)
             self._add(key, value)
             return value
+    def get(self, key, value):
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            self._remove(key)
+            self._add(key)
+            return value
