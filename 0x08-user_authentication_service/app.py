ae84b1e71da7054db19e64aa40b901d67007a6df
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-02-21 20:13:57 +0100

    app

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+
+
+#!/usr/bin/env python3
+""" API endpoints
+"""
+from flask import Flask, jsonify, request, abort
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root() -> str:
+    ''' root route '''
+    return jsonify({'message': 'Bienvenue'})
