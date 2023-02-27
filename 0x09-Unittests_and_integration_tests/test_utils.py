71235ce51bc83ca4f875306864dd920ae65962d9
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-02-27 22:51:21 +0100

    parameterized

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,11 @@ class TestAccessNestedMap(unittest.TestCase):
         """method to test that access_nested_map method
         returns what it is supposed to."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map:
+                                         Mapping, path: Sequence,
+                                         expected):
+                                         
+        """test that a KeyError is raised"""
+        with self.assertRaises(KeyError) as raises:
+            access_nested_map(nested_map, path)
+
