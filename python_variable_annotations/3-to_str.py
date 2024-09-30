fa10d0f95dbb5bcc8a03edf5b1576be8198842d4
MOUHAMEDelHakim <mohamedelhakim7@gmail.com>
2024-09-30 16:07:50 +0100

    update

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""" Write a type-annotated function
+that takes a float n as argument
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str function
+    """
+    return str(n)
